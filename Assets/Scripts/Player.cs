337c3d395c861e8f4d1d0335c5028431a8bd4bfd
ddayin <ddayin@hotmail.com>
2019-06-02 14:56:20 +0900

    플레이어 상태 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// 플레이어의 상태
+    /// </summary>
+    public enum PlayerState
     {
-        Debug.Log( "Start" );
+        Idle = 0,   // 대기 상태
+        Walk,       // 이동 상태
+        Stick       // 막대기를 늘리고 있는 상태
     }
 
-    // Update is called once per frame
+    public PlayerState m_State;
+
+    void Start()
+    {
+        m_State = PlayerState.Idle;
+    }
+
     void Update()
     {
-        Debug.Log( "Update" );
+        if ( Input.GetMouseButtonDown( 0 ) == true )
+        {
+            m_State = PlayerState.Stick;
+        }
+
+        if ( Input.GetMouseButtonUp( 0 ) == true )
+        {
+            m_State = PlayerState.Idle;
+        }
+
+        Debug.Log( m_State.ToString() );
     }
 }
