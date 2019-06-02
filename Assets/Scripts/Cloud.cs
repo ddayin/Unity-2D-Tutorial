9e42c69a48b47255d31dccacbbc928a52183bf2d
ddayin <ddayin@hotmail.com>
2019-06-02 16:40:01 +0900

    이동하는 구름 추가

---
+++ Assets/Scripts/Cloud.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cloud : MonoBehaviour
+{
+    public float m_Speed = 0.01f;    // 이동 속도
+    public bool m_MoveRight = true; // 오른쪽으로 이동하는 것인지
+
+    void Start()
+    {
+ 
+    }
+
+    void Update()
+    {
+        // x 축으로 이동할 위치
+        Vector3 newPosition = transform.position;
+        newPosition.x = newPosition.x + m_Speed;
+
+        // 구름의 위치 변경
+        transform.SetPositionAndRotation( newPosition, Quaternion.identity );
+    }
+}
