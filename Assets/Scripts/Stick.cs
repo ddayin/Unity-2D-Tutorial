cd86f392abedfc7105f8d1651f23b69ff10ea179
ddayin <ddayin@hotmail.com>
2019-06-04 21:16:07 +0900

    막대기 자라나도록 추가

---
+++ Assets/Scripts/Stick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stick : MonoBehaviour
+{
+    public float m_GrowSpeed = 10.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if ( Input.GetMouseButton( 0 ) == true )
+        {
+            // Y 축으로 변경할 값
+            float growY = transform.localScale.y + m_GrowSpeed * Time.deltaTime;
+
+            // 실제로 변경된 값으로 막대기 크기 조정
+            transform.localScale = new Vector3(transform.localScale.x, growY, transform.localScale.z);
+        }
+    }
+}
